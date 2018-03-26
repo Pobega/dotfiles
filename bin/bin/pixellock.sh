6ca71516242398907ecc95d4c97aa9b8629041e8
Michael Pobega <mpobega@neverware.com>
2018-03-26 16:54:28 -0400

    Removed outdated things I no longer use
    I have switched to GNOME3 on Wayland, so a bunch of X stuff is now useless.

--- bin/bin/pixellock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sleep .5
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 20% -scale 500% /tmp/screen.png
-[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-i3lock -u -i /tmp/screen.png
--- bin/bin/popup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-(echo " $@"; sleep 3) | lemonbar -B \#262626 -g 240x80+10+10
