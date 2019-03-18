ea3f8d9bf92e7b9759a758ff11d5215c52093836
Michael Pobega <pobega@gmail.com>
2019-03-18 11:15:35 -0400

    fish: avoiding checking git info if not in git repo

--- fish/.config/fish/conf.d/git.fish
+++ fish/.config/fish/conf.d/git.fish
@@ -18,17 +18,22 @@ function in_git_status
   return 1
 end
 
+function git_is_repo -d "Check if directory is a repository"
+  test -d .git; or command git rev-parse --git-dir >/dev/null 2> /dev/null
+end
+
 function set_git_color --on-event fish_prompt
-  echo $__fish_conf_dir
-  if in_git_status C         # Clean
-    set __fish_git_prompt_color_branch green --bold
-  else if in_git_status d    # Dirty
-    set __fish_git_prompt_color_branch red --bold
-  else if in_git_status c    # Conflicted
-    set __fish_git_prompt_color_branch yellow --bold
-  else if in_git_status u    # Untracked
-    set __fish_git_prompt_color_branch red
-  else if in_git_status s    # Staged
-    set __fish_git_prompt_color_branch purple
+  if git_is_repo
+    if in_git_status C         # Clean
+      set __fish_git_prompt_color_branch green --bold
+    else if in_git_status d    # Dirty
+      set __fish_git_prompt_color_branch red --bold
+    else if in_git_status c    # Conflicted
+      set __fish_git_prompt_color_branch yellow --bold
+    else if in_git_status u    # Untracked
+      set __fish_git_prompt_color_branch red
+    else if in_git_status s    # Staged
+      set __fish_git_prompt_color_branch purple
+    end
   end
 end
