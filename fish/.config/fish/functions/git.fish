dd72a10b1009e1158156dc4d466034af13a42526
Michael Pobega <pobega@gmail.com>
2019-03-18 20:26:49 -0400

    fish: fix git prompt coloring

--- fish/.config/fish/functions/git.fish
+++ fish/.config/fish/functions/git.fish
@@ -6,45 +6,48 @@ end
 set -g __fish_git_prompt_showdirtystate 1
 set -g __fish_git_prompt_showstashstate 1
 set -g __fish_git_prompt_showuntrackedfiles 1
-set -g __fish_git_prompt_showupstream 1
 
 set -g __fish_git_prompt_color_dirtystate red
 set -g __fish_git_prompt_color_stagedstate purple
-set -g __fish_git_prompt_color_stashstate yellow
-set -g __fish_git_prompt_color_untrackedfiles red
-set -g __fish_git_prompt_color_upstream green
+set -g __fish_git_prompt_color_stashstate blue
+set -g __fish_git_prompt_color_untrackedfiles yellow
 
 set -g __fish_git_prompt_char_dirtystate "?"
 set -g __fish_git_prompt_char_untrackedfiles "!"
 
-function in_git_prompt
+function in_git_prompt -d "Check if argv[1] is in the git_prompt"
   if string match -q -- "*$argv[1]*" $git_prompt
     return 0
   end
   return 1
 end
 
+function is_clean_upstream -d "True if we match upstream"
+  if string match -q -- "=" (__fish_git_prompt_show_upstream)
+    return 0
+  end
+  return 1
+end
+
 # https://github.com/oh-my-fish/oh-my-fish/blob/master/lib/git/git_is_repo.fish
-function git_is_repo -d "Check if directory is a repository"
+function is_git_repo -d "Check if directory is a repository"
   test -d .git; or command git rev-parse --git-dir >/dev/null 2> /dev/null
 end
 
-function set_git_colors
-  set -g __fish_git_prompt_color_branch $argv
-end
-
 function set_git_color
-  set -l git_prompt (__fish_git_prompt)
-  if git_is_repo
+  if is_git_repo
+    set -g git_prompt (__fish_git_prompt)
     # Branch colors ordered by priority
-    if in_git_prompt "?"       # Dirty
-      set_git_colors red
-    else if in_git_prompt "!"  # Untracked
-      set_git_colors blue
-    else if in_git_prompt "+"  # Staged
-      set_git_colors purple
-    else                       # Clean
-      set_git_colors green
+    if in_git_prompt "\?"
+      set -g __fish_git_prompt_color_branch $__fish_git_prompt_color_dirtystate
+    else if in_git_prompt "!"
+      set -g __fish_git_prompt_color_branch $__fish_git_prompt_color_untrackedfiles
+    else if in_git_prompt "+"
+      set -g __fish_git_prompt_color_branch $__fish_git_prompt_color_stagedstate
+    else if is_clean_upstream
+      set -g __fish_git_prompt_color_branch green --bold
+    else
+      set -g __fish_git_prompt_color_branch green
     end
   end
 end
