1137f3084c54c14facc4adc2fe0b138910e69c1a
Michael Pobega <pobega@gmail.com>
2025-12-06 17:48:56 -0500

    nvim: switch to CodeCompanion, remove Avante

---
+++ nvim/.config/nvim/lua/plugins/codecompanion-setup.lua
@@ -0,0 +1,26 @@
+return {
+  require("codecompanion").setup({
+    strategies = {
+--      inline = {
+--        adapter = "copilot",
+--      },
+      inline = {
+        adapter = "gemini_cli",
+      },
+    },
+    adapters = {
+      acp = {
+        gemini_cli = function()
+          return require("codecompanion.adapters").extend("gemini_cli", {
+            defaults = {
+              auth_method = "gemini-api-key",
+            },
+--            env = {
+--              GEMINI_API_KEY = "cmd:op read op://personal/Gemini_API/credential --no-newline",
+--            },
+          })
+        end,
+      },
+    },
+  }),
+}
---
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,18 @@
+return {
+  "olimorris/codecompanion.nvim",
+  opts = {},
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "ravitemer/mcphub.nvim",
+    "MeanderingProgrammer/render-markdown.nvim",
+      ft = { "markdown", "codecompanion" },
+    "echasnovski/mini.diff",
+      config = function()
+	local diff = require("mini.diff")
+	diff.setup({
+	  -- Disabled by default
+	  source = diff.gen_source.none(),
+	})
+      end,
+  },
+}
