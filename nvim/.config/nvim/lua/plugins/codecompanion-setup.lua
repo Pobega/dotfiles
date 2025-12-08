711ffdc8ae701e42293c01975acc5c29fff84120
Michael Pobega <pobega@gmail.com>
2025-12-08 11:58:03 -0500

    nvim: expand CodeCompanion for Mistral Codestral use

--- nvim/.config/nvim/lua/plugins/codecompanion-setup.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion-setup.lua
@@ -2,22 +2,57 @@ return {
   require("codecompanion").setup({
     strategies = {
       inline = {
-        adapter = "copilot",
+        adapter = "mistral",
+        model = 'codestral-latest',
+        keymaps = {
+          accept_change = {
+            modes = { n = "ga" },
+            description = "Accept the suggested change",
+          },
+          reject_change = {
+            modes = { n = "gr" },
+            opts = { nowait = true },
+            description = "Reject the suggested change",
+          },
+          stop = {
+            modes = { n = "q" },
+            index = 4,
+            callback = "keymaps.stop",
+            description = "Stop request",
+          },
+        },
       },
-      inline = {
-        adapter = "gemini_cli",
+      chat = {
+        adapter = 'mistral',
+        model = 'codestral-latest',
+        tools = {
+          opts = {
+            -- Needed to use tools in Mistral
+            auto_submit_errors = true,
+          },
+        },
       },
     },
     adapters = {
+      http = {
+        mistral = function()
+          return require("codecompanion.adapters").extend("mistral", {
+            env = {
+              api_key = "MISTRAL_API_KEY",
+            },
+          })
+        end,
+      },
       acp = {
+        opts = { show_defaults = false, },
         gemini_cli = function()
           return require("codecompanion.adapters").extend("gemini_cli", {
             defaults = {
               auth_method = "gemini-api-key",
             },
---            env = {
---              GEMINI_API_KEY = "cmd:op read op://personal/Gemini_API/credential --no-newline",
---            },
+            env = {
+              api_key = "GEMINI_API_KEY",
+            },
           })
         end,
       },
--- nvim/.config/nvim/lua/plugins/mappings.lua
+++ nvim/.config/nvim/lua/plugins/mappings.lua
@@ -8,7 +8,18 @@ return {
         ["<leader>bn"] = { "<cmd>bnext<cr>", desc = "Next buffer" },
         -- CodeCompanion Hotkeys
         ["<Leader>a"] = { desc = "CodeCompanion" },
-        ["<leader>aa"] = { "<cmd>CodeCompanionActions<cr>", desc = "Code Companion Action Palette" },
+        ["<leader>aa"] = { "<cmd>CodeCompanion<cr>", desc = "Code Companion Prompt" },
+        ["<leader>as"] = { "<cmd>CodeCompanionActions<cr>", desc = "Code Companion Action Palette" },
+        ["<leader>ac"] = { "<cmd>CodeCompanionChat<cr>", desc = "Code Companion Chat Window" },
+      },
+      v = {
+        -- CodeCompanion Visual Mode Hotkeys
+        -- TODO: These currently do not work as the buffer isn't properly sent into them.
+        -- The goal here is to have the buffer silently passed in visual mode, but have the
+        -- user only see the 'Prompt' popup (for example.)
+        ["<Leader>a"] = { desc = "CodeCompanion" },
+        ["<leader>aa"] = { "<cmd>CodeCompanion<cr>", desc = "Code Companion Prompt" },
+        ["<leader>as"] = { "<cmd>CodeCompanionActions<cr>", desc = "Code Companion Action Palette" },
         ["<leader>ac"] = { "<cmd>CodeCompanionChat<cr>", desc = "Code Companion Chat Window" },
       },
     },
