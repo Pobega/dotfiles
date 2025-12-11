4fd394e6236cf20b52c0e7ccfb10a1b9573a8a65
Michael Pobega <pobega@gmail.com>
2025-12-10 19:49:47 -0500

    neovim: add a bunch of configuration for CodeCompanion.

--- nvim/.config/nvim/lua/plugins/codecompanion-setup.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion-setup.lua
@@ -1,5 +1,56 @@
 return {
   require("codecompanion").setup({
+    extensions = {
+      spinner = {},
+    },
+    display = {
+      diff = {
+        provider_opts = {
+          inline = {
+            layout = "float", -- float|buffer - Where to display the diff
+            opts = {
+              context_lines = 3, -- Number of context lines in hunks
+              dim = 25, -- Background dim level for floating diff (0-100, [100 full transparent], only applies when layout = "float")
+              full_width_removed = true, -- Make removed lines span full width
+              show_keymap_hints = true, -- Show "gda: accept | gdr: reject" hints above diff
+              show_removed = true, -- Show removed lines as virtual text
+            },
+          },
+        },
+      },
+      chat = {
+        fold_context = false, -- Fold context messages in chat buffer
+        window = {
+          layout = "vertical", -- float|vertical|horizontal|buffer
+          position = nil, -- left|right|top|bottom (nil will default depending on vim.opt.splitright|vim.opt.splitbelow)
+          border = "single",
+          height = 0.8,
+          width = 0.40,
+          relative = "editor",
+          full_height = true, -- when set to false, vsplit will be used to open the chat buffer vs. botright/topleft vsplit
+          sticky = false, -- when set to true and `layout` is not `"buffer"`, the chat buffer will remain opened when switching tabs
+          opts = {
+            breakindent = true,
+            cursorcolumn = false,
+            cursorline = false,
+            foldcolumn = "0",
+            linebreak = true,
+            list = false,
+            numberwidth = 1,
+            signcolumn = "no",
+            spell = false,
+            wrap = true,
+          },
+        },
+        ---Customize how tokens are displayed
+        ---@param tokens number
+        ---@param adapter CodeCompanion.Adapter
+        ---@return string
+        token_count = function(tokens, adapter)
+          return " (" .. tokens .. " tokens)"
+        end,
+      },
+    },
     strategies = {
       inline = {
         adapter = "mistral",
@@ -25,16 +76,29 @@ return {
       chat = {
         adapter = 'mistral',
         model = 'codestral-latest',
+        display = {
+          chat = {
+            intro_message = "Welcome to CodeCompanion ✨!\n Press ? for options",
+            separator = "─", -- The separator between the different messages in the chat buffer
+            show_context = true, -- Show context (from slash commands and variables) in the chat buffer?
+            show_header_separator = false, -- Show header separators in the chat buffer? Set this to false if you're using an external markdown formatting plugin
+            show_settings = true, -- Show LLM settings at the top of the chat buffer?
+            show_token_count = true, -- Show the token count for each response?
+            show_tools_processing = true, -- Show the loading message when tools are being executed?
+            start_in_insert_mode = false, -- Open the chat buffer in insert mode?
+          },
+        },
         tools = {
           opts = {
-            -- Needed to use tools in Mistral
-            auto_submit_errors = true,
+            default_tools = { "full_stack_dev" },
+            auto_submit_errors = true, -- Needed to use tools with Mistral
           },
         },
       },
     },
     adapters = {
       http = {
+        opts = { show_defaults = false, },
         mistral = function()
           return require("codecompanion.adapters").extend("mistral", {
             env = {
@@ -42,19 +106,19 @@ return {
             },
           })
         end,
+        copilot = function()
+          return require("codecompanion.adapters").extend("copilot", {})
+        end,
       },
       acp = {
         opts = { show_defaults = false, },
-        gemini_cli = function()
-          return require("codecompanion.adapters").extend("gemini_cli", {
-            defaults = {
-              auth_method = "gemini-api-key",
-            },
+        opencode = function()
+          return require("codecompanion.adapters").extend("opencode", {
             env = {
-              api_key = "GEMINI_API_KEY",
+              api_key = "MISTRAL_API_KEY",
             },
           })
-        end,
+        end
       },
     },
   }),
--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -3,15 +3,26 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "ravitemer/mcphub.nvim",
-    "MeanderingProgrammer/render-markdown.nvim",
-      ft = { "markdown", "codecompanion" },
+    "franco-ruggeri/codecompanion-spinner.nvim",
+    "j-hui/fidget.nvim",
+    "saghen/blink.cmp",
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+        ft = { "markdown", "codecompanion" },
+        opts = {
+        render_modes = true,
+        sign = {
+          enabled = false,
+        },
+      },
+    },
     "echasnovski/mini.diff",
-      config = function()
-	local diff = require("mini.diff")
-	diff.setup({
-	  -- Disabled by default
-	  source = diff.gen_source.none(),
-	})
-      end,
+    config = function()
+	    local diff = require("mini.diff")
+	    diff.setup({ source = diff.gen_source.none(), })
+    end,
   },
+  init = function()
+    require("plugins.codecompanion.fidget-spinner"):init()
+  end,
 }
