ca292a8d64af4c899664e41c1a76deedfad844ec
Michael Pobega <pobega@google.com>
2026-04-29 21:03:01 +0000

    nvim: add ctrl-t shortcut to show/hide horizontal terminal

--- nvim/.config/nvim/lua/plugins/mappings.lua
+++ nvim/.config/nvim/lua/plugins/mappings.lua
@@ -6,6 +6,7 @@ return {
       n = {
         -- Buffer hotkeys
         ["<leader>bn"] = { "<cmd>bnext<cr>", desc = "Next buffer" },
+        ["<C-t>"] = { "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Toggle terminal" },
         ["<Leader>s"] = { desc = " Sidekick" },
         ["<leader>sa"] = { function() require("sidekick.cli").toggle() end, desc = "Sidekick Toggle CLI", },
         ["<leader>ss"] = { function() require("sidekick.cli").select() end, desc = "Select CLI", },
@@ -15,6 +16,9 @@ return {
         ["<leader>sp"] = { function() require("sidekick.cli").prompt() end, desc = "Sidekick Select Prompt", },
         ["<leader>sc"] = { function() require("sidekick.cli").toggle({ name = "copilot", focus = true }) end, desc = "Sidekick Toggle Copilot", },
         ["<leader>sg"] = { function() require("sidekick.cli").toggle({ name = "gemini", focus = true }) end, desc = "Sidekick Toggle Gemini", },
+      },
+      t = {
+        ["<C-t>"] = { "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Toggle terminal" },
       },
       v = {
         ["<Leader>s"] = { desc = " Sidekick" },
